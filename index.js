bdcf2389699f3dd05e4b7ac9788e7d9feaa9f16b
Edward Castillo <castilloedwarda@gmail.com>
2019-08-18 01:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
  } else if (string === "HELLO") {
    return "YES INDEED!";
- } else if (string === "I love you, Grandma") {
+ } 
+   if (string === "I love you, Grandma") {
    return "I love you, too.";
  }
 }
